e93510db6e68bde4095f35dbb30b675f3378598b
Nikita Lossinov <nikita.lossinov@gmail.com>
2023-08-25 22:41:06 +0600

    Добавлен эндпоинт для получения информации о версии бэкенда

--- eco-almaty-api/src/main/kotlin/kz/enactus/ecoalmaty/api/EcoAlmatyApiApplication.kt
+++ eco-almaty-api/src/main/kotlin/kz/enactus/ecoalmaty/api/EcoAlmatyApiApplication.kt
@@ -2,9 +2,22 @@ package kz.enactus.ecoalmaty.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
+@RestController
 @SpringBootApplication
-class EcoAlmatyApiApplication
+class EcoAlmatyApiApplication {
+
+    private val dateTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd-SSSSSS")
+    private val version: String = dateTimeFormatter.format(LocalDateTime.now())
+
+    @GetMapping("/api/v1/version")
+    fun getVersion(): String = version
+
+}
 
 fun main(args: Array<String>) {
     runApplication<EcoAlmatyApiApplication>(*args)
