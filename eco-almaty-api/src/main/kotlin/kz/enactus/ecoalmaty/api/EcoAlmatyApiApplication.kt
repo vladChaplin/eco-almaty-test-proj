c07d63cf6d46e0fca47fb834f29cb21d4e5327ed
Nikita Lossinov <nikita.lossinov@gmail.com>
2023-08-25 22:35:04 +0600

    eco-almaty-api initial commit

---
+++ eco-almaty-api/src/main/kotlin/kz/enactus/ecoalmaty/api/EcoAlmatyApiApplication.kt
@@ -0,0 +1,11 @@
+package kz.enactus.ecoalmaty.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EcoAlmatyApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<EcoAlmatyApiApplication>(*args)
+}
---
+++ eco-almaty-api/src/test/kotlin/kz/enactus/ecoalmaty/api/EcoAlmatyApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package kz.enactus.ecoalmaty.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EcoAlmatyApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
